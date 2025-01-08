d96f7786b75f4ada064cf2691912f4f7cd350817
yanjinlkham9 <bilgunn93@gmail.com>
2025-01-08 11:08:37 +0900

    ClassProps example

--- 21-props-state/src/App.js
+++ 21-props-state/src/App.js
@@ -1,5 +1,14 @@
+import { ClassProps, ClassProps2 } from "./components/ClassProps";
+
 function App() {
-  return <div className="App">hello react</div>;
+  return (
+    <div>
+      <h3>class형 props 사용해보기</h3>
+      <ClassProps name="루피" color="pink" nickname="공주" />
+
+      <ClassProps2 name="루피" color="pink" nickname="공주" fontColor="white" />
+    </div>
+  );
 }
 
 export default App;
---
+++ 21-props-state/src/components/ClassProps.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+
+class ClassProps extends Component {
+  render() {
+    const divStyle = {
+      color: this.props.color,
+    };
+    return (
+      <div style={divStyle}>
+        <h4>Hi, {this.props.name}</h4>
+        <ul>
+          <li>별명: {this.props.nickname} </li>
+          <li>좋아하는 색: {this.props.color} </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class ClassProps2 extends Component {
+  render() {
+    //props n object 형태-r irne
+    const { name, color, nickname, fontColor } = this.props;
+    const divStyle = {
+      color: fontColor,
+      backgroundColor: color,
+    };
+    return (
+      <div style={divStyle}>
+        <h4>Hi, {name}</h4>
+        <ul>
+          <li>별명: {nickname} </li>
+          <li>좋아하는 색: {color} </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export { ClassProps, ClassProps2 };
