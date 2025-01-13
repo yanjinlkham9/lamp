ff46ba0288c7fc1e651002d02510fae8df87db60
yanjinlkham9 <bilgunn93@gmail.com>
2025-01-13 10:31:31 +0900

    ref Class형

---
+++ 22-ref-lifecycle/src/App.jsx
@@ -0,0 +1,11 @@
+import { RefClass1, RefClass2 } from "./components/RefClass";
+function App() {
+  return (
+    <div>
+      <RefClass1 />
+      <RefClass2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ 22-ref-lifecycle/src/components/RefClass.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export class RefClass1 extends Component {
+  handleFocus = () => {
+    console.log(this.myInput);
+    console.log(this.myInput.value);
+    this.myInput.focus();
+  };
+  render() {
+    return (
+      <div>
+        <p>class형 Component, button click시 input에 focus 처리</p>
+        <p>ref 속성에 콜백 전달</p>
+        <input
+          type="text"
+          ref={(ref) => {
+            this.myInput = ref;
+          }}
+        />
+        <button onClick={this.handleFocus}>focus</button>
+      </div>
+    );
+  }
+}
+
+export class RefClass2 extends Component {
+  //createRef()를 사용해서 ref 객체를 사용
+  myInput = React.createRef();
+
+  handleFocus = () => {
+    console.log(this.myInput.current);
+    console.log(this.myInput.current.value);
+    this.myInput.current.focus();
+  };
+  render() {
+    return (
+      <div>
+        <p>class형 Component, button click시 input에 focus 처리</p>
+        <p>createRef() 를 통해서 ref 객체 생성</p>
+        <input type="text" ref={this.myInput} />
+        <button onClick={this.handleFocus}>focus</button>
+      </div>
+    );
+  }
+}
