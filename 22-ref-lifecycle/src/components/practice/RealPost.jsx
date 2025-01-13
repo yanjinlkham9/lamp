dce0e02a4cdd71380d5a4b5fa9c100911a528421
yanjinlkham9 <bilgunn93@gmail.com>
2025-01-13 14:58:53 +0900

    react lifecycle practice

--- 22-ref-lifecycle/src/components/practice/RealPost.jsx
+++ 22-ref-lifecycle/src/components/practice/RealPost.jsx
@@ -4,6 +4,8 @@ import PostItem from "./PostItem";
 export default function RealPost() {
   //https://jsonplaceholder.typicode.com/posts
   const [posts, setPosts] = useState([]);
+
+  //   1. async await 사용해서 useEffect
   const getPosts = async () => {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
@@ -12,9 +14,19 @@ export default function RealPost() {
     setPosts(response.data.slice(0, 10));
   };
 
+  //2. useEffect 안에서 then. catch 이용
   useEffect(() => {
-    getPosts();
+    // getPosts();
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setPosts(res.data.slice(0, 5));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
   return (
     <>
       <h6>여기는 RealPost</h6>
