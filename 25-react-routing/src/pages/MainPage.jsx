0ed2508c332840a1f664fb3bed0fb5aa0124ff7f
yanjinlkham9 <bilgunn93@gmail.com>
2025-01-17 16:16:39 +0900

    react router

--- 25-react-routing/src/pages/MainPage.jsx
+++ 25-react-routing/src/pages/MainPage.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 export default function MainPage() {
+  const [searchQuery, setSearchQuery] = useSearchParams();
+  console.log(searchQuery.get("mode")); //Dark, null
   return (
-    <main>
+    // <main className={["MainPage", searchQuery.get("mode")].join(" ")}>
+    <main className={`MainPage ${searchQuery.get("mode")}`}>
       <p>여기는 홈입니다. 🎆</p>
+      <button onClick={() => setSearchQuery({ mode: "Dark" })}>Darkmode</button>
     </main>
   );
 }
